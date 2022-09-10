3309c63664d042e0de3df8403b572a6448d38a6a
preegnees <mrraziapov13@gmail.com>
2022-09-11 00:38:34 +0300

    ...

--- pkg/storage/storage_test.go
+++ pkg/storage/storage_test.go
@@ -8,6 +8,8 @@ import (
 
 	m "simpleMemStor/pkg/model"
 	"testing"
+
+	"go.uber.org/goleak"
 )
 
 var stor m.IStreamStorage
@@ -23,8 +25,10 @@ func TestMain(m *testing.M) {
 }
 
 func TestSavePeersAndGetPeers(t *testing.T) {
+
+	defer goleak.VerifyNone(t)
 	var idCh1 m.IdChannel = "1"
-	// var idCh2 m.IdChannel = "2"
+	var idCh2 m.IdChannel = "2"
 
 	data := []struct {
 		Name string
@@ -33,7 +37,7 @@ func TestSavePeersAndGetPeers(t *testing.T) {
 		{"1", m.Peer{IdChannel: idCh1, Name: "N1", GrpcStream: "test1"}},
 		{"2", m.Peer{IdChannel: idCh1, Name: "N2", GrpcStream: "test2"}},
 		{"3", m.Peer{IdChannel: idCh1, Name: "N3", GrpcStream: "test3"}},
-		// {"4", m.Peer{IdChannel: idCh2, Name: "N4", GrpcStream: "test4"}},
+		{"4", m.Peer{IdChannel: idCh2, Name: "N4", GrpcStream: "test4"}},
 	}
 
 	chs := make([]<-chan map[m.Peer]struct{}, 0)
@@ -42,7 +46,6 @@ func TestSavePeersAndGetPeers(t *testing.T) {
 		chs = append(chs, stor.SavePeer(d.Peer))
 	}
 	
-
 	if len(chs) == 0 {
 		fmt.Println("Длинна массива chs = 0")
 	}
@@ -56,7 +59,7 @@ func TestSavePeersAndGetPeers(t *testing.T) {
 	}
 
 	var wg sync.WaitGroup
-	wg.Add(len(chs) + 1)
+	wg.Add(len(chs))
 
 	for i, ch := range chs {
 		go func (i int, ch <-chan map[m.Peer]struct{})  {
@@ -76,43 +79,12 @@ func TestSavePeersAndGetPeers(t *testing.T) {
 		}(i, ch)
 	}
 
-	time.Sleep(1 * time.Second)
-	err := stor.DeletePeer(m.Peer{IdChannel: idCh1, Name: "N1", GrpcStream: "test1"})
-	if err != nil {
-		t.Error(err)
-	}
-
-	time.Sleep(1 * time.Second)
-	i := 5
-	myPeer := m.Peer{IdChannel: idCh1, Name: "N3000", GrpcStream: "test3000"}
-	ch := stor.SavePeer(myPeer)
-
-	go func (i int, ch <-chan map[m.Peer]struct{})  {
-		for {
-			select {
-			case val, ok := <-ch:
-				if ok {
-					incr()
-					fmt.Println("index=", i, ", val=", val)	
-					continue
-				}
-				wg.Done()
-				fmt.Println("index=", i, ", closed")
-				return
-			}
-		}
-	}(i, ch)
-
-	time.Sleep(4 * time.Second)
 	for _, d := range data {
-		stor.CloseChan(d.Peer)
+		time.Sleep(1 * time.Second)
+		stor.DeletePeer(d.Peer)
+		fmt.Println(d.Peer)
 	}
-	stor.CloseChan(myPeer)
 	wg.Wait()
 
-	fmt.Println(count)
-
-	if count != len(chs) {
-		t.Error("не равно")
-	}
+	fmt.Println("---->", count)
 }
