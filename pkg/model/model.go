f96ab7efb80d71fcfe361b5cdc5b22f43e3e500c
preegnees <mrraziapov13@gmail.com>
2022-09-11 13:50:16 +0300

    добавил разрешенные имена в структура пира

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -13,12 +13,14 @@ type Token string
 Peer. Пир - это подключение, которое имеет:
 IdChannel: id канала, с которым он связан;
 Name: имя, которое имеет пир;
+AllowedNames: разрешенные имена, с которым хочет соединится пир. Перечислять нужно через ","
 GrpcStream=стрим gprc;
 */
 type Peer struct {
-	IdChannel  IdChannel
-	Name       Name
-	GrpcStream interface{}
+	IdChannel    IdChannel
+	Name         Name
+	AllowedNames string
+	GrpcStream   interface{}
 }
 
 /*
@@ -27,6 +29,6 @@ SavePeer: сохранение пира;
 DeletePeer: удаление пира при отключении;
 */
 type IStreamStorage interface {
-	SavePeer(peer Peer) <-chan map[Peer]struct{}
+	SavePeer(peer Peer) (<-chan map[Peer]struct{}, error)
 	DeletePeer(peer Peer) error
 }
